1f0a7cdcd2e89d7344655dbe7662abc34b7882e3
Afeefa Ansari <ansariafeefa7860@gmail.com>
2026-05-05 11:39:29 +0530

    simplify template structure and improve weather data display UI

--- weather/views.py
+++ weather/views.py
@@ -7,16 +7,20 @@ import urllib.request
 def index(request):
     if request.method == 'POST':
         city = request.POST.get('city')
-        req = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' +city+ '&appid=685cc9e89c1734c32384a424251c55a0').read()
-        json_data = json.loads(req)
+        try:
+            req = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' +city+ '&appid=685cc9e89c1734c32384a424251c55a0').read()
+            json_data = json.loads(req)
+        except:
+            return render(request, 'weather/index.html', {'error': 'City not found'})
         temp = json_data['main']['temp'] - 273.15
         data = {
             'country_code': str(json_data['sys']['country']),
             'city': city,
+            'description': str(json_data['weather'][0]['description']),
             'coordinate': str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
             'temp': str(round(temp, 2)) + ' °C',
-            'pressure': str(json_data['main']['pressure']),
-            'humidity': str(json_data['main']['humidity']),
+            'pressure': str(json_data['main']['pressure']) + ' hPa',
+            'humidity': str(json_data['main']['humidity']) + ' %',
         }
         return render(request, 'weather/index.html' , {'data': data})
     return render(request, 'weather/index.html')
