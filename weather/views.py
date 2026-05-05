fd3bd9e78c2a6aaec247133b87cbfa95e0770802
Afeefa Ansari <ansariafeefa7860@gmail.com>
2026-05-05 11:04:48 +0530

    Add OpenWeather API integration to fetch and display weather data

--- weather/views.py
+++ weather/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 
 def index(request):
     if request.method == 'POST':
         city = request.POST.get('city')
-        context = {'city': city}
-        return render(request, 'weather/index.html', {
-            'context': context
-        })
+        req = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' +city+ '&appid=685cc9e89c1734c32384a424251c55a0').read()
+        json_data = json.loads(req)
+        data = {
+            'country_code': str(json_data['sys']['country']),
+            'city': city,
+            'coordinate': str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            'temp': str(json_data['main']['temp']),
+            'pressure': str(json_data['main']['pressure']),
+            'humidity': str(json_data['main']['humidity']),
+        }
+        return render(request, 'weather/index.html' , {'data': data})
     return render(request, 'weather/index.html')
